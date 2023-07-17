ed59df4607446b79a494aebdf5231db38a28be1d
Milkev <kmdre@hotmail.com>
2023-07-16 22:32:19 -0600

    Added Shulker Charm to Tools item group

--- src/main/java/space/bbkr/shulkercharm/ShulkerCharm.java
+++ src/main/java/space/bbkr/shulkercharm/ShulkerCharm.java
@@ -5,7 +5,9 @@ import io.github.ladysnake.pal.Pal;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -25,6 +27,11 @@ public class ShulkerCharm implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		AutoConfig.register(ShulkerCharmConfig.class, JanksonConfigSerializer::new);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
+			content.add(SHULKER_CHARM);
+		});
+
 		config = AutoConfig.getConfigHolder(ShulkerCharmConfig.class).getConfig();
 		if (config.maxEnergy != -1) {
 			config.maxPower = config.maxEnergy;
